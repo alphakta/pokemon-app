3ed4121b5e549f466776c56ccb98552f25193cbb
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2022-09-10 21:49:04 +0200

    add service

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -9,13 +9,16 @@ import { Pokemon } from '../pokemon';
   styles: [
   ]
 })
-export class ListPokemonComponent {
-  pokemonList: Pokemon[] = POKEMONS
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[] | undefined;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
 
   goToPokemon(pokemon: Pokemon){
     this.router.navigate(['/pokemon', pokemon.id])
   }
 
+  ngOnInit(){
+    this.pokemonList = this.pokemonService.getPokemonList()
+  }
 }
