3d4d06d131078cacaba49decb4f492fee9d0d238
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2022-09-10 19:02:07 +0200

    add components & templates

---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html'
+})
+export class DetailPokemonComponent implements OnInit {
+
+  pokemonList: Pokemon[] | undefined
+  pokemon: Pokemon | undefined
+
+  constructor(private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.pokemonList = POKEMONS
+    const pokemonId: string|null = this.router.snapshot.paramMap.get('id')
+
+    if(pokemonId)
+      this.pokemon = this.pokemonList.find((pokemon) => pokemon.id == Number(pokemonId))
+
+  }
+
+}
